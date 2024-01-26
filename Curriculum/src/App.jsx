4c5d858213e63bc928d6eecea90b9fdeaf938477
Diegolpz18 <diego.jared.leyva@cecytesinaloa.edu.mx>
2024-01-26 10:06:36 -0700

    Nuevo Cambio Muchachos

--- Curriculum/src/App.jsx
+++ Curriculum/src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+function App() {
+  return (
+    <>
+    <h1>Curriculum</h1>
+    </>
+  )
+}
+
+export default App
