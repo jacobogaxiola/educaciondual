6b66b20eb817ee6370a09d74818da108000d7a15
Diegolpz18 <diego.jared.leyva@cecytesinaloa.edu.mx>
2024-01-26 11:38:59 -0700

    MVAC

--- Curriculum/src/App.jsx
+++ Curriculum/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
+    <container>
     <h1>Curriculum</h1>
+    </container>
     </>
   )
 }
--- Curriculum/src/main.jsx
+++ Curriculum/src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Diego from './App.jsx'
+import Luis from './App.jsx'
+import Francisco from './App.jsx'
+import Isis from './App.jsx'
+import Jassiel from './App.jsx'
+import Yan from './App.jsx'
+import Lenin from './App.jsx'
+import Paulina from './App.jsx'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,5 +19,13 @@ import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Diego />
+    <Luis />
+    <Francisco />
+    <Isis />
+    <Jassiel />
+    <Yan />
+    <Lenin />
+    <Paulina />
   </React.StrictMode>,
 )
