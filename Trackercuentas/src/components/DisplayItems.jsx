b8896a79699c7186710bbbeedc978c0493b1c693
jassiel-amx <jassiel.gastelum@cecytesinaloa.edu.mx>
2024-02-01 11:01:56 -0700

    s

---
+++ Trackercuentas/src/components/DisplayItems.jsx
@@ -0,0 +1,24 @@
+import SimgleItems from "./SimgleItem";
+
+const DisplayItems = ({ subs, eliminarItem, editItem }) => {
+    return (
+        <>
+	    <h2>Suscripciones</h2>
+	    {
+		subs.map(item => (
+		     <SimgleItem
+		     key={item.id}
+		     id={item.id}
+		     prince={item.prince}
+		     type={item.type}
+		     eliminarItem={eliminarItem}
+		     editItem={editItem}
+		     />
+		))
+
+	    }
+        </>
+    );
+}
+
+export default DisplayItem;
---
+++ Trackercuentas/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+ 	<div className="header">
+	    <h2>subscriptions traker</h2>
+	</div>
+    );
+}
+
+export default Header;
