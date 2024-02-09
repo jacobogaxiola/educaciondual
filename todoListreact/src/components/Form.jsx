546934596833f8398d5db36eb4f5b7a6f22a9a82
Luisgael1622 <luis.gael.gutierrez@cecytesinaloa.edu.mx>
2024-02-09 11:31:38 -0600

    asasa

--- todoListreact/src/components/Form.jsx
+++ todoListreact/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Form = {( handleChange, addTask, tarea)} => {
+const Form = ({ handleChange, addTask, tarea}) => {
     return (
         <div>
             <form onSubmit={addTask}>
