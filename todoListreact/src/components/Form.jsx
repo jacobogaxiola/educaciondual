3b002edb51772317a5c9b86bb7f04d1eae789e45
Luisgael1622 <luis.gael.gutierrez@cecytesinaloa.edu.mx>
2024-02-09 11:30:36 -0600

    asa

---
+++ todoListreact/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Form = {( handleChange, addTask, tarea)} => {
+    return (
+        <div>
+            <form onSubmit={addTask}>
+                <input type="text" value={tarea} placeholder="ingresa tarea..." onChange={handleChange}/>
+                <button type="submit">Agregar tarea</button>
+            </form>
+        </div>
+    )
+}
+
+export {Form}
